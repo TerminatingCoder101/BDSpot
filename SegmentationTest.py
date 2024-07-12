b9590eb5c61e5ded4f19dd78679816183d9dec7d
Sagar <ganesha4us1@gmail.com>
2024-07-12 10:35:14 -0400

    Initial commit

---
+++ SegmentationTest.py
@@ -0,0 +1,175 @@
+import argparse
+import sys
+import time
+
+import cv2
+import numpy as np
+
+import bosdyn.client
+import bosdyn.client.estop
+import bosdyn.client.lease
+import bosdyn.client.util
+from bosdyn.api import estop_pb2, geometry_pb2, image_pb2, manipulation_api_pb2
+from bosdyn.client.estop import EstopClient
+from bosdyn.client.frame_helpers import VISION_FRAME_NAME, get_vision_tform_body, math_helpers
+from bosdyn.client.image import ImageClient
+from bosdyn.client.manipulation_api_client import ManipulationApiClient
+from bosdyn.client.robot_command import RobotCommandClient, blocking_stand
+from bosdyn.client.robot_state import RobotStateClient
+
+g_image_click = None
+g_image_display = None
+
+
+def verify_estop(robot):
+    """Verify the robot is not estopped"""
+
+    client = robot.ensure_client(EstopClient.default_service_name)
+    if client.get_status().stop_level != estop_pb2.ESTOP_LEVEL_NONE:
+        error_message = 'Robot is estopped. Please use an external E-Stop client, such as the' \
+                        ' estop SDK example, to configure E-Stop.'
+        robot.logger.error(error_message)
+        raise Exception(error_message)
+
+
+def segment_setting(config):
+    """A simple example of using the Boston Dynamics API to command Spot's arm."""
+
+    # See hello_spot.py for an explanation of these lines.
+    bosdyn.client.util.setup_logging(config.verbose)
+
+    sdk = bosdyn.client.create_standard_sdk('ArmObjectGraspClient')
+    robot = sdk.create_robot(config.hostname)
+    bosdyn.client.util.authenticate(robot)
+    robot.time_sync.wait_for_sync()
+
+    assert robot.has_arm(), 'Robot requires an arm to run this example.'
+
+    # Verify the robot is not estopped and that an external application has registered and holds
+    # an estop endpoint.
+    verify_estop(robot)
+
+    lease_client = robot.ensure_client(bosdyn.client.lease.LeaseClient.default_service_name)
+    image_client = robot.ensure_client(ImageClient.default_service_name)
+
+    manipulation_api_client = robot.ensure_client(ManipulationApiClient.default_service_name)
+
+    with bosdyn.client.lease.LeaseKeepAlive(lease_client, must_acquire=True, return_at_exit=True):
+        # Now, we are ready to power on the robot. This call will block until the power
+        # is on. Commands would fail if this did not happen. We can also check that the robot is
+        # powered at any point.
+        robot.logger.info('Powering on robot... This may take a several seconds.')
+        robot.power_on(timeout_sec=20)
+        assert robot.is_powered_on(), 'Robot power on failed.'
+        robot.logger.info('Robot powered on.')
+
+        # Tell the robot to stand up. The command service is used to issue commands to a robot.
+        # The set of valid commands for a robot depends on hardware configuration. See
+        # RobotCommandBuilder for more detailed examples on command building. The robot
+        # command service requires timesync between the robot and the client.
+        robot.logger.info('Commanding robot to stand...')
+        command_client = robot.ensure_client(RobotCommandClient.default_service_name)
+        blocking_stand(command_client, timeout_sec=10)
+        robot.logger.info('Robot standing.')
+
+        # Take a picture with a camera
+        robot.logger.info('Getting an image from: %s', config.image_source)
+        image_responses = image_client.get_image_from_sources([config.image_source])
+
+        if len(image_responses) != 1:
+            print(f'Got invalid number of images: {len(image_responses)}')
+            print(image_responses)
+            assert False
+
+        image = image_responses[0]
+
+        if image.shot.image.pixel_format == image_pb2.Image.PIXEL_FORMAT_DEPTH_U16:
+            dtype = np.uint16
+        else:
+            dtype = np.uint8
+
+        img = np.fromstring(image.shot.image.data, dtype=dtype)
+
+        image_cv = cv2.imdecode(img, cv2.IMREAD_COLOR)
+
+        hsv_image = cv2.cvtColor(image_cv, cv2.COLOR_BGR2HSV)
+
+        lower_red1 = np.array([0, 120, 70])
+        upper_red1 = np.array([10, 255, 255])
+        mask_red1 = cv2.inRange(hsv_image, lower_red1, upper_red1)
+
+        lower_red2 = np.array([170, 120, 70])
+        upper_red2 = np.array([180, 255, 255])
+        mask_red2 = cv2.inRange(hsv_image, lower_red2, upper_red2)
+
+        # Combine the masks
+        mask_red = mask_red1 + mask_red2
+
+        # Bitwise-AND mask and original image
+        result_image = cv2.bitwise_and(image_cv, image_cv, mask=mask_red)
+
+        # Display the original and segmented images
+        cv2.imshow('Original Image', image_cv)
+        cv2.imshow('Segmented Image', result_image)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+        
+
+        time.sleep(4.0)
+
+        robot.logger.info('Sitting down and turning off.')
+
+        # Power the robot off. By specifying "cut_immediately=False", a safe power off command
+        # is issued to the robot. This will attempt to sit the robot before powering off.
+        robot.power_off(cut_immediately=False, timeout_sec=20)
+        assert not robot.is_powered_on(), 'Robot power off failed.'
+        robot.logger.info('Robot safely powered off.')
+
+
+
+def main():
+    """Command line interface."""
+    parser = argparse.ArgumentParser()
+    bosdyn.client.util.add_base_arguments(parser)
+    parser.add_argument('-i', '--image-source', help='Get image from source',
+                        default='frontleft_fisheye_image')
+    parser.add_argument('-t', '--force-top-down-grasp',
+                        help='Force the robot to use a top-down grasp (vector_alignment demo)',
+                        action='store_true')
+    parser.add_argument('-f', '--force-horizontal-grasp',
+                        help='Force the robot to use a horizontal grasp (vector_alignment demo)',
+                        action='store_true')
+    parser.add_argument(
+        '-r', '--force-45-angle-grasp',
+        help='Force the robot to use a 45 degree angled down grasp (rotation_with_tolerance demo)',
+        action='store_true')
+    parser.add_argument('-s', '--force-squeeze-grasp',
+                        help='Force the robot to use a squeeze grasp', action='store_true')
+    options = parser.parse_args()
+
+    num = 0
+    if options.force_top_down_grasp:
+        num += 1
+    if options.force_horizontal_grasp:
+        num += 1
+    if options.force_45_angle_grasp:
+        num += 1
+    if options.force_squeeze_grasp:
+        num += 1
+
+    if num > 1:
+        print('Error: cannot force more than one type of grasp.  Choose only one.')
+        sys.exit(1)
+
+    try:
+        segment_setting(options)
+        return True
+    except Exception as exc:  # pylint: disable=broad-except
+        logger = bosdyn.client.util.get_logger()
+        logger.exception('Threw an exception')
+        return False
+
+
+if __name__ == '__main__':
+    if not main():
+        sys.exit(1)
