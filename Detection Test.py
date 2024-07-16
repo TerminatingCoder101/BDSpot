a7abb3743f0317c7496b18fd1a2887615ac03456
Sagar <ganesha4us1@gmail.com>
2024-07-16 16:32:39 -0400

    Update Detection and new image

--- Detection Test.py
+++ Detection Test.py
@@ -4,181 +4,162 @@ import time
 
 import cv2
 import numpy as np
+from scipy import ndimage
 from skimage.morphology import skeletonize, dilation, square
-from skimage import data
 
 import bosdyn.client
-import bosdyn.client.estop
-import bosdyn.client.lease
 import bosdyn.client.util
-from bosdyn.api import estop_pb2, geometry_pb2, image_pb2, manipulation_api_pb2
-from bosdyn.client.estop import EstopClient
-from bosdyn.client.frame_helpers import VISION_FRAME_NAME, get_vision_tform_body, math_helpers
-from bosdyn.client.image import ImageClient
-from bosdyn.client.manipulation_api_client import ManipulationApiClient
-from bosdyn.client.robot_command import RobotCommandClient, blocking_stand
-from bosdyn.client.robot_state import RobotStateClient
+from bosdyn.api import image_pb2
+from bosdyn.client.image import ImageClient, build_image_request
 
-g_image_click = None
-g_image_display = None
-
-
-def verify_estop(robot):
-    """Verify the robot is not estopped"""
-
-    client = robot.ensure_client(EstopClient.default_service_name)
-    if client.get_status().stop_level != estop_pb2.ESTOP_LEVEL_NONE:
-        error_message = 'Robot is estopped. Please use an external E-Stop client, such as the' \
-                        ' estop SDK example, to configure E-Stop.'
-        robot.logger.error(error_message)
-        raise Exception(error_message)
+ROTATION_ANGLE = {
+    'back_fisheye_image': 0,
+    'frontleft_fisheye_image': -78,
+    'frontright_fisheye_image': -102,
+    'left_fisheye_image': 0,
+    'right_fisheye_image': 180
+}
 
 
-def segment_setting(config):
-    """A simple example of using the Boston Dynamics API to command Spot's arm."""
-
-    # See hello_spot.py for an explanation of these lines.
-    bosdyn.client.util.setup_logging(config.verbose)
-
-    sdk = bosdyn.client.create_standard_sdk('ArmObjectGraspClient')
-    robot = sdk.create_robot(config.hostname)
-    bosdyn.client.util.authenticate(robot)
-    robot.time_sync.wait_for_sync()
-
-    assert robot.has_arm(), 'Robot requires an arm to run this example.'
-
-    # Verify the robot is not estopped and that an external application has registered and holds
-    # an estop endpoint.
-    verify_estop(robot)
-
-    lease_client = robot.ensure_client(bosdyn.client.lease.LeaseClient.default_service_name)
-
-    image_client = robot.ensure_client(ImageClient.default_service_name)
-
-    manipulation_api_client = robot.ensure_client(ManipulationApiClient.default_service_name)
-
-    with bosdyn.client.lease.LeaseKeepAlive(lease_client, must_acquire=True, return_at_exit=True):
-        # Now, we are ready to power on the robot. This call will block until the power
-        # is on. Commands would fail if this did not happen. We can also check that the robot is
-        # powered at any point.
-        robot.logger.info('Powering on robot... This may take a several seconds.')
-        robot.power_on(timeout_sec=20)
-        assert robot.is_powered_on(), 'Robot power on failed.'
-        robot.logger.info('Robot powered on.')
-
-        # Tell the robot to stand up. The command service is used to issue commands to a robot.
-        # The set of valid commands for a robot depends on hardware configuration. See
-        # RobotCommandBuilder for more detailed examples on command building. The robot
-        # command service requires timesync between the robot and the client.
-        robot.logger.info('Commanding robot to stand...')
-        command_client = robot.ensure_client(RobotCommandClient.default_service_name)
-        blocking_stand(command_client, timeout_sec=10)
-        robot.logger.info('Robot standing.')
-
-        # Take a picture with a camera
-        robot.logger.info('Getting an image from: %s', config.image_source)
-        image_responses = image_client.get_image_from_sources([config.image_source])
-
-        if len(image_responses) != 1:
-            print(f'Got invalid number of images: {len(image_responses)}')
-            print(image_responses)
-            assert False
-
-        image = image_responses[0]
-
-        if image.shot.image.pixel_format == image_pb2.Image.PIXEL_FORMAT_DEPTH_U16:
-            dtype = np.uint16
-        else:
-            dtype = np.uint8
-
-        img = np.fromstring(image.shot.image.data, dtype=dtype)
-
-        image_cv = cv2.imdecode(img, cv2.IMREAD_COLOR)
-
-        hsv_image = cv2.cvtColor(image_cv, cv2.COLOR_BGR2HSV)
-    
-        lower_red1 = np.array([0, 120, 70])
-        upper_red1 = np.array([10, 255, 255])
-        mask_red1 = cv2.inRange(hsv_image, lower_red1, upper_red1)
-
-        lower_red2 = np.array([170, 120, 70])
-        upper_red2 = np.array([180, 255, 255])
-        mask_red2 = cv2.inRange(hsv_image, lower_red2, upper_red2)
-
-        mask_red = mask_red1 + mask_red2
-
-        result_image = cv2.bitwise_and(img, img, mask=mask_red)
-
-        gray_img = cv2.cvtColor(result_image, cv2.COLOR_RGB2GRAY)
-        
-        _, black_white = cv2.threshold(gray_img, 1, 255, cv2.THRESH_BINARY)
-
-        # Normalize the image to range [0, 1] for skeletonization
-        black_white_norm = black_white // 255
-
-        skel_img = skeletonize(black_white_norm).astype(np.uint8)
-
-        dilated_skeleton = dilation(skel_img, square(3))
-        cv2.imshow('Original', image_cv)
-        cv2.imshow('Final', dilated_skeleton)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
-
-        time.sleep(4.0)
-
-        robot.logger.info('Sitting down and turning off.')
-
-        # Power the robot off. By specifying "cut_immediately=False", a safe power off command
-        # is issued to the robot. This will attempt to sit the robot before powering off.
-        robot.power_off(cut_immediately=False, timeout_sec=20)
-        assert not robot.is_powered_on(), 'Robot power off failed.'
-        robot.logger.info('Robot safely powered off.')
+def pixel_format_type_strings():
+    names = image_pb2.Image.PixelFormat.keys()
+    return names[1:]
 
 
+def pixel_format_string_to_enum(enum_string):
+    return dict(image_pb2.Image.PixelFormat.items()).get(enum_string)
 
-def main():
-    """Command line interface."""
+
+def main(argv):
+    # Parse args
     parser = argparse.ArgumentParser()
     bosdyn.client.util.add_base_arguments(parser)
-    parser.add_argument('-i', '--image-source', help='Get image from source',
-                        default='frontleft_fisheye_image')
-    parser.add_argument('-t', '--force-top-down-grasp',
-                        help='Force the robot to use a top-down grasp (vector_alignment demo)',
+    parser.add_argument('--list', help='list image sources',
                         action='store_true')
-    parser.add_argument('-f', '--force-horizontal-grasp',
-                        help='Force the robot to use a horizontal grasp (vector_alignment demo)',
+    parser.add_argument('--auto-rotate', help='rotate right and front images to be upright',
                         action='store_true')
+    parser.add_argument('--image-sources',
+                        help='Get image from source(s)', action='append')
+    parser.add_argument('--image-service', help='Name of the image service to query.',
+                        default=ImageClient.default_service_name)
     parser.add_argument(
-        '-r', '--force-45-angle-grasp',
-        help='Force the robot to use a 45 degree angled down grasp (rotation_with_tolerance demo)',
-        action='store_true')
-    parser.add_argument('-s', '--force-squeeze-grasp',
-                        help='Force the robot to use a squeeze grasp', action='store_true')
+        '--pixel-format', choices=pixel_format_type_strings(),
+        help='Requested pixel format of image. If supplied, will be used for all sources.')
+
     options = parser.parse_args()
 
-    num = 0
-    if options.force_top_down_grasp:
-        num += 1
-    if options.force_horizontal_grasp:
-        num += 1
-    if options.force_45_angle_grasp:
-        num += 1
-    if options.force_squeeze_grasp:
-        num += 1
+    # Create robot object with an image client.
+    sdk = bosdyn.client.create_standard_sdk('image_capture')
+    robot = sdk.create_robot(options.hostname)
+    bosdyn.client.util.authenticate(robot)
+    robot.sync_with_directory()
+    robot.time_sync.wait_for_sync()
 
-    if num > 1:
-        print('Error: cannot force more than one type of grasp.  Choose only one.')
-        sys.exit(1)
+    image_client = robot.ensure_client(options.image_service)
 
-    try:
-        segment_setting(options)
-        return True
-    except Exception as exc:  # pylint: disable=broad-except
-        logger = bosdyn.client.util.get_logger()
-        logger.exception('Threw an exception')
-        return False
+    # Raise exception if no actionable argument provided
+    if not options.list and not options.image_sources:
+        parser.error(
+            'Must provide actionable argument (list or image-sources).')
+
+    # Optionally list image sources on robot.
+    if options.list:
+        image_sources = image_client.list_image_sources()
+        print('Image sources:')
+        for source in image_sources:
+            print('\t' + source.name)
+    counter = 0
+    # Optionally capture one or more images.
+    while True:
+        if options.image_sources:
+            # Capture and save images to disk
+            pixel_format = pixel_format_string_to_enum(options.pixel_format)
+            image_request = [
+                build_image_request(source, pixel_format=pixel_format)
+                for source in options.image_sources
+            ]
+            image_responses = image_client.get_image(image_request)
+    
+            for image in image_responses:
+                num_bytes = 1  # Assume a default of 1 byte encodings.
+                if image.shot.image.pixel_format == image_pb2.Image.PIXEL_FORMAT_DEPTH_U16:
+                    dtype = np.uint16
+                    extension = '.png'
+                else:
+                    if image.shot.image.pixel_format == image_pb2.Image.PIXEL_FORMAT_RGB_U8:
+                        num_bytes = 3
+                    elif image.shot.image.pixel_format == image_pb2.Image.PIXEL_FORMAT_RGBA_U8:
+                        num_bytes = 4
+                    elif image.shot.image.pixel_format == image_pb2.Image.PIXEL_FORMAT_GREYSCALE_U8:
+                        num_bytes = 1
+                    elif image.shot.image.pixel_format == image_pb2.Image.PIXEL_FORMAT_GREYSCALE_U16:
+                        num_bytes = 2
+                    dtype = np.uint8
+                    extension = '.jpg'
+    
+                img = np.frombuffer(image.shot.image.data, dtype=dtype)
+                if image.shot.image.format == image_pb2.Image.FORMAT_RAW:
+                    try:
+                        # Attempt to reshape array into an RGB rows X cols shape.
+                        img = img.reshape(
+                            (image.shot.image.rows, image.shot.image.cols, num_bytes))
+                    except ValueError:
+                        # Unable to reshape the image data, trying a regular decode.
+                        img = cv2.imdecode(img, -1)
+                else:
+                    img = cv2.imdecode(img, -1)
+    
+                if options.auto_rotate:
+                    img = ndimage.rotate(img, ROTATION_ANGLE[image.source.name])
+
+
+                hsv_image = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+                lower_red1 = np.array([0, 120, 70])
+                upper_red1 = np.array([10, 255, 255])
+                mask_red1 = cv2.inRange(hsv_image, lower_red1, upper_red1)
+
+                lower_red2 = np.array([170, 120, 70])
+                upper_red2 = np.array([180, 255, 255])
+                mask_red2 = cv2.inRange(hsv_image, lower_red2, upper_red2)
+
+                mask_red = mask_red1 + mask_red2
+
+                result_image = cv2.bitwise_and(img, img, mask=mask_red)
+
+                gray_img = cv2.cvtColor(result_image, cv2.COLOR_RGB2GRAY)
+                
+                _, black_white = cv2.threshold(gray_img, 1, 255, cv2.THRESH_BINARY)
+
+                
+                cv2.imshow('Original', img)
+                cv2.imshow('Final', black_white)
+                cv2.waitKey(15000)
+                cv2.destroyAllWindows()
+
+                time.sleep(4.0)
+            
+                # Save the image from the GetImage request to the current directory with the filename
+                # matching that of the image source.
+                image_saved_path = image.source.name
+                # image_saved_path = image_saved_path.replace(
+                #     '/', '')  # Remove any slashes from the filename the image is saved at locally.
+                cv2.imwrite('C:\\Users\\chapmanm\\Desktop\\Ribbon_images\\'+ image_saved_path + str(counter) + extension, img)
+                print("Wrote image: ", image_saved_path)
+                counter += 1
+                time.sleep(1)
+                
+    return True
 
 
 if __name__ == '__main__':
-    if not main():
+    # main()
+    # sys.argv = ['--image-source','frontright_fisheye_image','192.168.80.3']
+    sys.argv = ['get_image.py', '--image-sources', 'hand_color_image',
+                '--pixel-format', 'PIXEL_FORMAT_RGB_U8', '192.168.80.3']
+
+    print(sys.argv)
+    if not main(sys.argv[1:]):
         sys.exit(1)
+
+
